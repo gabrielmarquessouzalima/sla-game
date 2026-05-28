2748fe84848e4af45f8739ab76f78ea09f7e09c3
gagazin <gabriel.souza.lima12@escola.pr.gov.br>
2026-05-28 11:37:44 -0300

    sla

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ imgPlayerCorrendo.onload = function() {
 // --- Configurações dos NPCs ---
 const npc = {
     x: 2500, 
-    y: 288,          
+    y: 300,          
     largura: 60,     
     altura: 80,      
     tempoFlutuar: 0, // Controla a velocidade da flutuação
