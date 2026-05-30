834a03ca930025ae85b4e63607b9e9adcb363bdc
gagazin <gabriel.souza.lima12@escola.pr.gov.br>
2026-05-30 18:41:57 -0300

    sla

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ const npc = {
 // --- RAPOSINHA SIMPLIFICADA (SEM HITBOX) ---
 const npc2 = {
     x: 5000, 
-    y: 295, // Posição Y direta. Ajuste aqui se precisar subir ou descer ela!
+    y: 280, // Posição Y direta. Ajuste aqui se precisar subir ou descer ela!
     largura: 110, 
     altura: 110
 };
